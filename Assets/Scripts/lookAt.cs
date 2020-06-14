eb642ee0bc4bbf4f1a33da285b558da60805053b
lokeshkosuri <55837754+lokeshkosuri@users.noreply.github.com>
2020-06-15 00:25:21 +0530

    Initial Commit 2

---
+++ Assets/Scripts/lookAt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAt : MonoBehaviour
+{
+    public Camera arCamera;
+
+    private void Start()
+    {
+        arCamera = Camera.main;
+    }
+    private void OnEnable()
+    {
+        arCamera = Camera.main;
+    }
+    void Update()
+    {
+        //Rotate the camera every frame so it keeps looking at the target
+        transform.LookAt(arCamera.transform);
+        //Same as above, but setting the worldUp parameter to Vector3.left in this example turns the camera on its side
+        //transform.LookAt(arCamera.transform, Vector3.down);
+    }
+}
